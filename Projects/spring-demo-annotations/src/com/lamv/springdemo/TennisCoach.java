9d4ffa8312eb53d67b816ceb182f5740ce742973
vuonglam <vuonglam.94@gmail.com>
2020-10-21 22:16:19 +0700

    Section8: Spring Configuration with Java Annotation - Dependency Injection: field injection

--- Projects/spring-demo-annotations/src/com/lamv/springdemo/TennisCoach.java
+++ Projects/spring-demo-annotations/src/com/lamv/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 	
+	@Autowired
 	private FortuneService fortuneService;
 	
 	// define a default constructor
@@ -30,11 +31,13 @@ public class TennisCoach implements Coach {
 	*/
 	
 	// define a method
+	/*
 	@Autowired
 	public void doSomeCrazyStuff(FortuneService theFortuneService) {
 		System.out.println(">> TennisCoach: doSomeCrazyStuff() method");
 		fortuneService = theFortuneService;
 	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
