f14cd372006f3df91428e8fb43c199165e38fdf3
vuonglam <vuonglam.94@gmail.com>
2020-10-21 16:44:45 +0700

    Section6: Spring Bean Lifecycle: init-method & destroy-method

---
+++ Projects/spring-demo-one/src/com/lamv/springdemo/BeanLifeCycleDemoApp.java
@@ -0,0 +1,21 @@
+package com.lamv.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp {
+
+	public static void main(String[] args) {
+		
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+		
+		// retrieve bean from spring container (application context)
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		System.out.println(theCoach.getDailyWorkout());
+		
+		// close the context
+		context.close();
+	}
+	
+}
