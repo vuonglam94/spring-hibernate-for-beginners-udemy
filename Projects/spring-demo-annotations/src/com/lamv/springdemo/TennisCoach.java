b44d5ce6a75a7b100caa08b6bb8dc9936e680138
vuonglam <vuonglam.94@gmail.com>
2020-10-21 21:17:47 +0700

    Section7: Spring Configuration with Java Annotation - Inversion of Control: default bean id

--- Projects/spring-demo-annotations/src/com/lamv/springdemo/TennisCoach.java
+++ Projects/spring-demo-annotations/src/com/lamv/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.lamv.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
