f30d11f488bed186ac8da44f1e36cb63f7a97655
vuonglam <vuonglam.94@gmail.com>
2020-10-30 16:25:10 +0700

    Section14: Spring MVC - Form Tags and Data Binding: Checkboxes & Radio Buttons

--- Projects/spring-mvc-demo/src/com/lamv/springdemo/mvc/Student.java
+++ Projects/spring-mvc-demo/src/com/lamv/springdemo/mvc/Student.java
@@ -7,6 +7,8 @@ public class Student {
 	private String firstName;
 	private String lastName;
 	private String country;
+	private String favoriteLanguage;
+	private String[] operatingSystems;
 	
 	private LinkedHashMap<String, String> countryOptions;
 	
@@ -48,6 +50,22 @@ public class Student {
 
 	public LinkedHashMap<String, String> getCountryOptions() {
 		return countryOptions;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
 	}
 	
 }
