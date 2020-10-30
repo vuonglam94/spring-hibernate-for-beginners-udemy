5fa467a3ce8a7c2d746ccc0a976a85cead07c7ce
vuonglam <vuonglam.94@gmail.com>
2020-10-30 16:02:22 +0700

    Section14: Spring MVC - Form Tags and Data Binding: Drop-Down Lists

--- Projects/spring-mvc-demo/WebContent/WEB-INF/view/student-form.jsp
+++ Projects/spring-mvc-demo/WebContent/WEB-INF/view/student-form.jsp
@@ -19,6 +19,15 @@
 		
 		<br><br>
 		
+		Country:
+		<form:select path="country">
+		
+			<form:options items="${student.countryOptions}" />
+			
+		</form:select>
+		
+		<br><br>
+		
 		<input type="submit" value="Submit" />
 	
 	</form:form>
--- Projects/spring-mvc-demo/src/com/lamv/springdemo/mvc/Student.java
+++ Projects/spring-mvc-demo/src/com/lamv/springdemo/mvc/Student.java
@@ -1,11 +1,24 @@
 package com.lamv.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 	
 	private String firstName;
 	private String lastName;
+	private String country;
+	
+	private LinkedHashMap<String, String> countryOptions;
 	
 	public Student() {
+		
+		// populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<String, String>();
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "French");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("US", "United States of America");
 		
 	}
 
@@ -23,6 +36,18 @@ public class Student {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
 	}
 	
 }
