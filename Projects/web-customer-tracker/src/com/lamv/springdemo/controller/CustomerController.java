5003cacc511dabbe0fb476eb276fbcb7a170ae07
vuonglam <vuonglam.94@gmail.com>
2020-11-02 23:49:08 +0700

    Section28: Build a Database Web App - Spring MVC and Hibernate Project: setup project

---
+++ Projects/web-customer-tracker/src/com/lamv/springdemo/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package com.lamv.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@RequestMapping("/list")
+	public String listCustomer(Model theModel) {
+		
+		return "customer-lists";
+	}
+	
+}
