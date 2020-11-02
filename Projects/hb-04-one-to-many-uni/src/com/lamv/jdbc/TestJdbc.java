310c8c0a3c0c1c28064df2307eeb1648309bb855
vuonglam <vuonglam.94@gmail.com>
2020-11-02 11:37:16 +0700

    Section26: Hibernate Advanced Mappings: @OneToMany - Unidirectional

---
+++ Projects/hb-04-one-to-many-uni/src/com/lamv/jdbc/TestJdbc.java
@@ -0,0 +1,27 @@
+package com.lamv.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb-04-one-to-many-uni?useSSL=false";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			System.out.println("Connection successful!!!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
