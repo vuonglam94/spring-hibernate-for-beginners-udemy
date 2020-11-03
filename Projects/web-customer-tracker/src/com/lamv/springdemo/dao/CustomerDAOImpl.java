052af67f7d75ef91c6b6c003acfe7941d23faec8
vuonglam <vuonglam.94@gmail.com>
2020-11-03 18:09:53 +0700

    Section32: Build a Database Web App - Spring MVC and Hibernate Project: update customer

--- Projects/web-customer-tracker/src/com/lamv/springdemo/dao/CustomerDAOImpl.java
+++ Projects/web-customer-tracker/src/com/lamv/springdemo/dao/CustomerDAOImpl.java
@@ -38,9 +38,21 @@ public class CustomerDAOImpl implements CustomerDAO {
 		// get the current hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
 		
-		// save the customer
-		currentSession.save(theCustomer);
+		// save/update the customer
+		currentSession.saveOrUpdate(theCustomer);
 		
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		// retrieve/read from database using the primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		return theCustomer;
 	}
 
 }
