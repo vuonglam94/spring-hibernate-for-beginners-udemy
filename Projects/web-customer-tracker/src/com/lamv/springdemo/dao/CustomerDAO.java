2e94bc711c57e3b81d940085dead6c4c73299ad0
vuonglam <vuonglam.94@gmail.com>
2020-11-03 16:51:23 +0700

    Section31: Build a Database Web App - Spring MVC and Hibernate Project: add customer

--- Projects/web-customer-tracker/src/com/lamv/springdemo/dao/CustomerDAO.java
+++ Projects/web-customer-tracker/src/com/lamv/springdemo/dao/CustomerDAO.java
@@ -7,5 +7,7 @@ import com.lamv.springdemo.entity.Customer;
 public interface CustomerDAO {
 
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
 	
 }
--- Projects/web-customer-tracker/src/com/lamv/springdemo/service/CustomerService.java
+++ Projects/web-customer-tracker/src/com/lamv/springdemo/service/CustomerService.java
@@ -7,5 +7,7 @@ import com.lamv.springdemo.entity.Customer;
 public interface CustomerService {
 
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
 	
 }
