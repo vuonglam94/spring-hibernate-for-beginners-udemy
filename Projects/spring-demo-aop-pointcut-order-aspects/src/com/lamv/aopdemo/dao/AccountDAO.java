99b49ab037bd5ab2c543acc9ec6ca500b66ac123
vuonglam <vuonglam.94@gmail.com>
2020-11-24 22:56:13 +0700

    Section39: AOP: Ordering Aspects

---
+++ Projects/spring-demo-aop-pointcut-order-aspects/src/com/lamv/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,45 @@
+package com.lamv.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+import com.lamv.aopdemo.Account;
+
+@Component
+public class AccountDAO {
+	
+	private String name;
+	private String serviceCode;
+
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		
+		System.out.println(getClass() + " :DOING MY DB WORK: ADDING AN ACCOUNT");
+		
+	}
+	
+	public boolean doWork() {
+		
+		System.out.println(getClass() + " :doWork");
+		return false;
+	}
+
+	public String getName() {
+		System.out.println(getClass() + " :getName()");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + " :setName()");
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + " :getServiceCode()");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + " :setServiceCode()");
+		this.serviceCode = serviceCode;
+	}
+	
+}
