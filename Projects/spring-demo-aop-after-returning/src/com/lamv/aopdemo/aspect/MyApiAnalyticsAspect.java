adbeb3a411aff046dec0a1ae4bc4794cbdf89212
vuonglam <vuonglam.94@gmail.com>
2020-11-26 09:24:25 +0700

    Section41: AOP: @AfterReturning Advice Type

---
+++ Projects/spring-demo-aop-after-returning/src/com/lamv/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -0,0 +1,18 @@
+package com.lamv.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(3)
+public class MyApiAnalyticsAspect {
+	
+	@Before("com.lamv.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
+	public void performApiAnalytics() {
+		System.out.println("\n=====>>> Perform API Analytics");
+	}
+
+}
