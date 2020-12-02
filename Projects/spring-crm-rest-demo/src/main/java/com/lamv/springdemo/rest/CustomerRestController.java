5ab1ad997117f250d86e44c215ae4999ec5a9794
vuonglam <vuonglam.94@gmail.com>
2020-12-02 21:43:26 +0700

    Section66: Spring REST - CRUD Database Real-Time Project - Add Customer

--- Projects/spring-crm-rest-demo/src/main/java/com/lamv/springdemo/rest/CustomerRestController.java
+++ Projects/spring-crm-rest-demo/src/main/java/com/lamv/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +37,16 @@ public class CustomerRestController {
 		
 		return theCustomer;
 	}
+	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
 	
 }
