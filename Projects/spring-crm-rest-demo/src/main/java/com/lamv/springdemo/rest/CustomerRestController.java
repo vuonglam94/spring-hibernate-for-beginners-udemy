3caeb7f5f2e9c1ef9c2a4fb4ed613f0654acbc71
vuonglam <vuonglam.94@gmail.com>
2020-12-02 21:24:57 +0700

    Section65: Spring REST - CRUD Database Real-Time Project - Exception Handling

--- Projects/spring-crm-rest-demo/src/main/java/com/lamv/springdemo/rest/CustomerRestController.java
+++ Projects/spring-crm-rest-demo/src/main/java/com/lamv/springdemo/rest/CustomerRestController.java
@@ -29,6 +29,10 @@ public class CustomerRestController {
 		
 		Customer theCustomer = customerService.getCustomer(customerId);
 		
+		if (theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+		
 		return theCustomer;
 	}
 	
---
+++ Projects/spring-crm-rest-demo/src/main/java/com/lamv/springdemo/rest/CustomerRestExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.lamv.springdemo.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomerRestExceptionHandler {
+
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException exc) {
+
+		CustomerErrorResponse error = new CustomerErrorResponse(HttpStatus.NOT_FOUND.value(), exc.getMessage(),
+				System.currentTimeMillis());
+
+		return new ResponseEntity<CustomerErrorResponse>(error, HttpStatus.NOT_FOUND);
+
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(Exception exc) {
+
+		CustomerErrorResponse error = new CustomerErrorResponse(HttpStatus.BAD_REQUEST.value(), exc.getMessage(),
+				System.currentTimeMillis());
+
+		return new ResponseEntity<CustomerErrorResponse>(error, HttpStatus.BAD_REQUEST);
+
+	}
+
+}
