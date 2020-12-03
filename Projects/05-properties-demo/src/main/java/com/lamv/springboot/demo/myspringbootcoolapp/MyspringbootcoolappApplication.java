ec9b046de8cad80a888829e45cd83c49e652bfb9
vuonglam <vuonglam.94@gmail.com>
2020-12-03 10:54:55 +0700

    Section73: Spring Boot - Application Properties

---
+++ Projects/05-properties-demo/src/main/java/com/lamv/springboot/demo/myspringbootcoolapp/MyspringbootcoolappApplication.java
@@ -0,0 +1,13 @@
+package com.lamv.springboot.demo.myspringbootcoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringbootcoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringbootcoolappApplication.class, args);
+	}
+
+}
