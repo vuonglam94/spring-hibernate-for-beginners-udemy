2a44d313bf54bff462ae366425c8a58b8ade90eb
vuonglam <vuonglam.94@gmail.com>
2020-12-03 22:53:36 +0700

    Section74: Spring Boot - Build a REST CRUD API with Hibernate - Real-Time Project

---
+++ Projects/cruddemo/src/main/java/com/lamv/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.lamv.springboot.cruddemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lamv.springboot.cruddemo.dao.EmployeeDAO;
+import com.lamv.springboot.cruddemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	
+	private EmployeeDAO employeeDAO;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+		this.employeeDAO = employeeDAO;
+	}
+
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		return employeeDAO.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Employee findById(int theId) {
+		return employeeDAO.findById(theId);
+	}
+
+	@Override
+	@Transactional
+	public void save(Employee theEmployee) {
+		employeeDAO.save(theEmployee);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int theId) {
+		employeeDAO.deleteById(theId);
+	}
+
+}
