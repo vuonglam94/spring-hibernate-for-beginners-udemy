eebcaecd18ef90e6f39f65bcbfadb572450d4433
vuonglam <vuonglam.94@gmail.com>
2020-12-04 11:25:57 +0700

    Section79: Spring Boot - Thymeleaf - Build HTML Tables

---
+++ Projects/33-thymeleafdemo-employees-list-css/src/main/java/com/lamv/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,43 @@
+package com.lamv.springboot.thymeleafdemo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lamv.springboot.thymeleafdemo.model.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	private List<Employee> theEmployees;
+	
+	@PostConstruct
+	private void loadData() {
+		
+		Employee emp1 = new Employee(1, "Leslie", "Andrew", "leslie@luv2code.com");
+		Employee emp2 = new Employee(2, "Emma", "Baumgarten", "emma@luv2code.com");
+		Employee emp3 = new Employee(3, "Evani", "Gupta", "avani@luv2code.com");
+		
+		theEmployees = new ArrayList<Employee>();
+		
+		theEmployees.add(emp1);
+		theEmployees.add(emp2);
+		theEmployees.add(emp3);
+	}
+	
+	@GetMapping("/list")
+	public String listEmployee(Model theModel) {
+		
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "list-employees";
+	}
+	
+}
