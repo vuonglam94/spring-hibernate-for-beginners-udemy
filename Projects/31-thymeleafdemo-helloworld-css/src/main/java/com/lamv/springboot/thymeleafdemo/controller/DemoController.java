9955d5a6e0c124128df486c010552632144bf3e7
vuonglam <vuonglam.94@gmail.com>
2020-12-04 11:00:22 +0700

    Section78: Spring Boot - Thymeleaf

---
+++ Projects/31-thymeleafdemo-helloworld-css/src/main/java/com/lamv/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.lamv.springboot.thymeleafdemo.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/hello")
+	public String sayHello(Model theModel) {
+		
+		theModel.addAttribute("theDate", new Date());
+		
+		return "helloworld";
+	}
+	
+}
